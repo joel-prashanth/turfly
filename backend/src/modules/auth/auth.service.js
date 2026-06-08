1b0742efba016f3479d667544cb43895eeba2537
Joel <joel.prashanth0528@gmail.com>
2026-06-08 23:51:01 +0530

    feat(auth): implement user registration with bcrypt hashing

--- backend/src/modules/auth/auth.service.js
+++ backend/src/modules/auth/auth.service.js
@@ -1,17 +1,54 @@
 const prisma = require("../../config/prisma");
+const bcrypt = require("bcrypt");
 
 const register = async (userData) => {
   const { name, email, password, role } = userData;
 
-  console.log({
-    name,
-    email,
-    password,
-    role,
+  if (!name || !email || !password || !role) {
+    throw new Error("All fields are required");
+  }
+
+  //Role validation - only allow "OWNER" and "PLAYER"
+
+  const allowedRoles = ["OWNER", "PLAYER"];
+
+  if (!allowedRoles.includes(role)) {
+    throw new Error("Invalid role");
+  }
+
+  //Check for duplicate users based on email
+
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  if (existingUser) {
+    throw new Error("Email already exists");
+  }
+
+  //Hashing password
+
+  const saltRounds = 10;
+
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  //Create User
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      passwordHash,
+      role,
+    },
   });
 
   return {
-    message: "Register service reached",
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
   };
 };
 
